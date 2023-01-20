d496e40c4c257f803353967c3e90d133c60a6f41
sayedmisfar030 <123141754+sayedmisfar030@users.noreply.github.com>
2023-01-20 10:33:07 +0530

    Update and rename README.md to README.js

---
+++ README.js
@@ -0,0 +1,19 @@
+# sayed
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>JavaScript Arithmetic</h2>
+
+
+<p id="demo"></p>
+
+<script>
+let x = 100 + 50;
+document.getElementById("demo").innerHTML = x;
+</script>
+
+</body>
+</html>
+
+
